90bf1e300d3229b8c794580b7eed9c7bd49a217f
Yuri Kireev <70061113+JurgenKV@users.noreply.github.com>
2025-07-21 13:19:51 +0300

    ...

--- Parsers.py
+++ Parsers.py
@@ -11,6 +11,56 @@ import UniDate
 import LinkConst
 import WorkFilter
 
+def parsing_all_pages():
+    print(datetime.datetime.now().time())
+    all_tech_list = list()
+    HTMLTaker.initialize_driver()
+    all_tech_list.extend(parse_ERIP('ЕРИП'))
+    all_tech_list.extend(parse_BFT('БФТ'))
+    all_tech_list.extend(parse_BPC('БПЦ'))
+    all_tech_list.extend(parse_MNS('МНС'))
+    all_tech_list.extend(parse_OAIS('ОАИС'))
+    all_tech_list.extend(parse_A1('A1'))  # cringo
+    all_tech_list.extend(parse_MTS('МТС'))  # cringo
+    all_tech_list.extend(parse_Life('Life'))
+    all_tech_list.extend(parse_Seventech('Seventech'))
+    all_tech_list.extend(parse_Beltelecom('Beltelecom'))  # cringo
+    all_tech_list.extend(parse_Delova9Seti('Деловая Сеть'))
+    all_tech_list.extend(parse_Hoster('Hoster'))
+    all_tech_list.extend(parse_BeCloud('BeCloud'))
+    all_tech_list.extend(parse_Oplati('Оплати'))
+    all_tech_list.extend(parse_Kupala('Kupala'))
+    all_tech_list.extend(parse_BVFB('БВФБ'))
+    all_tech_list.extend(parse_NBRB('НБРБ'))
+    all_tech_list.extend(parse_Bank_AlfaRu('Альфа-Банк Россия'))
+    all_tech_list.extend(parse_Bank_Belarusbank('Беларусьбанк'))
+    all_tech_list.extend(parse_Bank_BSB('БСБ Банк'))
+    all_tech_list.extend(parse_Bank_BTA('БТА Банк'))
+    all_tech_list.extend(parse_Bank_BankReshenii('Банк Решений'))
+    all_tech_list.extend(parse_Bank_BELWEB('БЕЛВЕБ Банк'))
+    all_tech_list.extend(parse_Bank_BelAgro('Белагропромбанк'))
+    all_tech_list.extend(parse_Bank_Belinvest('Белинвестбанк'))  # need selenium timeout >20
+    all_tech_list.extend(parse_Bank_MTB('МТБ Банк'))
+    all_tech_list.extend(parse_Bank_Paritet('Паритетбанк'))
+    all_tech_list.extend(parse_Bank_Zepter('Цептер Банк'))
+    all_tech_list.extend(parse_Bank_Sberbank('Сбербанк'))  # problem with year after NY
+    all_tech_list.extend(parse_Bank_Priorbank('Приорбанк'))
+    HTMLTaker.quit_driver()
+
+    print(datetime.datetime.now().time())
+    #for notif in all_tech_list:
+        # print(f"Сервис: {notif.service_type}")
+        # print(f"Дата работы: {notif.date_of_work}")
+        # print(f"Дата публикации: {notif.publishing_date}")
+        # print(f"Заголовок: {notif.work_header}")
+        # print(f"Ссылка: {notif.link}")
+        # print(f"Описание: {notif.description}")
+        # print(f"{notif.service_type} -- {notif.publishing_date}")
+        #print(f"{notif.publishing_date} Сервис:  {notif.service_type} = {notif.work_header}")
+    filtered = WorkFilter.get_works_by_period(all_tech_list, 4)
+    filtered = WorkFilter.sort_by_nearest_work(filtered)
+    for notif in filtered:
+        print(f"{notif.publishing_date} Сервис:  {notif.service_type} = {notif.work_header} = {notif.link}")
 
 def is_contains_work_keywords(text):
     keywords = ['работы','технич','технологич', 'недоступ', 'планов']
@@ -908,56 +958,3 @@ def parse_Bank_Priorbank(service_name = 'service_name is null'):
             print(f"data Exception in {service_name}")
 
     return tech_data_list
-
-def parsing_all_pages():
-    print(datetime.datetime.now().time())
-    all_tech_list = list()
-    HTMLTaker.initialize_driver()
-    # all_tech_list.extend(parse_ERIP('ЕРИП'))
-    # all_tech_list.extend(parse_BFT('БФТ'))
-    # all_tech_list.extend(parse_BPC('БПЦ'))
-    # all_tech_list.extend(parse_MNS('МНС'))
-    # all_tech_list.extend(parse_OAIS('ОАИС'))
-    # all_tech_list.extend(parse_A1('A1'))  # cringo
-    # all_tech_list.extend(parse_MTS('МТС'))  # cringo
-    # all_tech_list.extend(parse_Life('Life'))
-    # all_tech_list.extend(parse_Seventech('Seventech'))
-    # all_tech_list.extend(parse_Beltelecom('Beltelecom'))  # cringo
-    # all_tech_list.extend(parse_Delova9Seti('Деловая Сеть'))
-    # all_tech_list.extend(parse_Hoster('Hoster'))
-    # all_tech_list.extend(parse_BeCloud('BeCloud'))
-    # all_tech_list.extend(parse_Oplati('Оплати'))
-    # all_tech_list.extend(parse_Kupala('Kupala'))
-    # all_tech_list.extend(parse_BVFB('БВФБ'))
-    # all_tech_list.extend(parse_NBRB('НБРБ'))
-    all_tech_list.extend(parse_Bank_AlfaRu('Альфа-Банк Россия'))
-    all_tech_list.extend(parse_Bank_Belarusbank('Беларусьбанк'))
-    all_tech_list.extend(parse_Bank_BSB('БСБ Банк'))
-    all_tech_list.extend(parse_Bank_BTA('БТА Банк'))
-    all_tech_list.extend(parse_Bank_BankReshenii('Банк Решений'))
-    all_tech_list.extend(parse_Bank_BELWEB('БЕЛВЕБ Банк'))
-    all_tech_list.extend(parse_Bank_BelAgro('Белагропромбанк'))  # need some fix
-    all_tech_list.extend(parse_Bank_Belinvest('Белинвестбанк'))  # need selenium timeout >20
-    all_tech_list.extend(parse_Bank_MTB('МТБ Банк'))
-    all_tech_list.extend(parse_Bank_Paritet('Паритетбанк'))
-    all_tech_list.extend(parse_Bank_Zepter('Цептер Банк'))
-    all_tech_list.extend(parse_Bank_Sberbank('Сбербанк'))  # problem with year after NY
-    all_tech_list.extend(parse_Bank_Priorbank('Приорбанк'))
-    HTMLTaker.quit_driver()
-
-    print(datetime.datetime.now().time())
-    # for notif in all_tech_list:
-    #     # print(f"Сервис: {notif.service_type}")
-    #     # print(f"Дата работы: {notif.date_of_work}")
-    #     # print(f"Дата публикации: {notif.publishing_date}")
-    #     # print(f"Заголовок: {notif.work_header}")
-    #     # print(f"Ссылка: {notif.link}")
-    #     # print(f"Описание: {notif.description}")
-    #     print(f"{notif.service_type} -- {notif.publishing_date}")
-    #     #print('-------------------')
-    filtered = list()
-    filtered = WorkFilter.get_works_by_period(all_tech_list, 2)
-
-    filtered = WorkFilter.sort_by_nearest_work(filtered)
-    for notif in filtered:
-        print(f"{notif.publishing_date} Сервис:  {notif.service_type} = {notif.work_header}")
