558ffdd20908ca6d13c42381ef254da4c733e618
JurgenKV <yura.kireev.vt@gmail.com>
2025-09-30 12:34:39 +0300

    Deleted tinyUrl, added build settings and win64 exe

--- TG_Bot/utils.py
+++ TG_Bot/utils.py
@@ -1,14 +1,15 @@
-import pyshorteners
+# import pyshorteners
 
 import LOG
 
 def shorten_url(url):
-    import pyshorteners.shorteners
+    # import pyshorteners.shorteners
 
     try:
-        shortener = pyshorteners.Shortener()
-        short_url = shortener.tinyurl.short(url)
-        return short_url
+        #shortener = pyshorteners.Shortener()
+        #short_url = shortener.tinyurl.short(url)
+        #return short_url
+        return url
     except Exception as e:
         LOG.error(f"Ошибка при сокращении ссылки: {e}")
         return url
